0c3d22e53f175ce03b2786073614d34634cf795f
Wilson Britten <wilsunb@gmail.com>
2018-10-21 20:56:54 -0600

    initial commit

---
+++ backend/src/main/kotlin/backend/Application.kt
@@ -0,0 +1,15 @@
+package backend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+
+    companion object {
+        @JvmStatic()
+        fun main(args: Array<String>) {
+            SpringApplication(Application::class.java).run(*args)
+        }
+    }
+}
